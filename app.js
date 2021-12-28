62a0772c18555aca1046525d6d6f2c9b692e6ab3
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2021-12-28 18:34:02 +0200

    navigation and start screen setup
    add navigation and start screen, add a few get requests

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+// get requests
+app.get("/", function(req, res){
+  res.render("home");
+})
+
+app.get("/login", function(req, res){
+  res.render("login");
+})
+
+app.get("/register", function(req, res){
+  res.render("register");
+})
+
 app.listen(3000, function(){
   console.log("Server running on port 3000");
 })
