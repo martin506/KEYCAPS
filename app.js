335a7ea673db352f3cee07dd0c9490a6601967f8
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2021-12-28 15:08:51 +0200

    add starting files
    create app.js a few ejs file and two folders, initialize project

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+app.listen(3000, function(){
+  console.log("Server running on port 3000");
+})
