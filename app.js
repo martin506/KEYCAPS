d9aeb8d2f8eda3e3d8b7bfb7535de0ade3f453f9
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2021-12-28 21:47:33 +0200

    complete login and contacts interface
    add login through oauth2, authentication works, made contacts interface

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -10,19 +18,100 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  googleID: String
+})
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/keycaps",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // get requests
 app.get("/", function(req, res){
   res.render("home");
 })
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/keycaps',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
 app.get("/login", function(req, res){
-  res.render("login");
+  if (!req.isAuthenticated()){
+    res.render("login");
+  } else{
+    res.render("login2")
+  }
+
 })
 
 app.get("/register", function(req, res){
-  res.render("register");
+  if (!req.isAuthenticated()){
+    res.render("register");
+  } else{
+    res.render("login2")
+  }
 })
+
+app.get("/contacts", function(req, res){
+  res.render("contact");
+})
+
+// post requests
+
+app.post("/message", function(req, res){
+
+})
+
+
+
+
 
 app.listen(3000, function(){
   console.log("Server running on port 3000");
